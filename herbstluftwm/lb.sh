9492f389105c6997db52a9f6a63dd1e26a2bf5e9
nan0s7 <scottwalters97@gmail.com>
2017-09-16 14:00:47 +1000

    Made it way more power efficient

--- herbstluftwm/lb.sh
+++ herbstluftwm/lb.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
 slp=0
+per=0
+cur=0
+smallest=1
+# battery, date
+declare -a delay=( 60 60 )
+
+del_ln=${#delay[@]}
+
+function get_smallest {
+	tmp=${delay[0]}
+	for i in `seq 1 $del_ln`; do
+		if [ "${delay[$i]}" -lt "$tmp" ]; then
+			tmp="${delay[$i]}"
+		fi
+	done
+	smallest=$tmp
+}
 
 function battery {
-	#check_time=60
-	if [ "$1" -eq 60 ]; then
+	if [ "$1" -eq 0 ]; then
+		per=$(acpi --battery | cut -d, -f2)
+	elif [ "$1" -eq ${delay[0]} ]; then
 		per=$(acpi --battery | cut -d, -f2)
 		slp=1
-		echo "$per"
-	elif [ "$1" -eq 0 ]; then
-		per=$(acpi --battery | cut -d, -f2)
-		echo "$per"
 	fi
-	#echo "$percentage"
 }
 
 function get_date {
-	current=$(date "+%a %b %d, %T")
-	echo -n "$current"
+	if [ "$1" -eq 0 ]; then
+		cur=$(date "+%a %b %d, %H:%M")
+	elif [ "$1" -eq ${delay[1]} ]; then
+		cur=$(date "+%a %b %d, %H:%M")
+		slp=1
+	fi
 }
 
+get_smallest
 
 while true; do
-	bat_per=$(battery $slp)
-	echo "%{c}$(get_date $slp)""%{r}$bat_per"
-	sleep 1
-	slp=$[ $slp + 1 ] done
+	battery $slp
+	get_date $slp
+	echo "%{c}$cur""%{r}$per"
+	slp=$[ $slp + 1 ]
+	sleep $smallest
 done
